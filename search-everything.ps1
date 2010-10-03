46ab9ceac4114df01ddcb731b76b455cb63b2a27
unknown <Jameel@.(none)>
2010-10-03 10:44:32 +0200

    Made the search everything script into a function so that it can be executed anywhere once loaded

--- search-everything.ps1
+++ search-everything.ps1
@@ -1,30 +1,36 @@
-param($searchVal)
-
-$objConnection = New-Object -ComObject adodb.connection
-$objrecordset = New-Object -ComObject adodb.recordset
-$objconnection.open("Provider=Search.CollatorDSO;Extended Properties='Application=Windows';")
-$objrecordset.open("SELECT System.ItemName, System.ItemTypeText, System.Size FROM SystemIndex", $objConnection)
-$objrecordset.MoveFirst()
-$GLOBAL:SE_results = @()
-
-write-host 'Searching'
-
-do
+function search-everything()
 {
-    $val = ($objrecordset.Fields.Item("System.ItemName"))
-    if($val.value.tostring() -match $searchVal){$GLOBAL:SE_results += $val;$val} 
-    #$objrecordset.Fields.Item("System.ITemTypeText")
-    #$objrecordset.Fields.Item("System.SIze")
-    $objrecordset.MoveNext()
-#    write-host '.' -NoNewLine
+    param($searchVal)
 
-} Until ($objrecordset.EOF)
+    $objConnection = New-Object -ComObject adodb.connection
+    $objrecordset = New-Object -ComObject adodb.recordset
+    $objconnection.open("Provider=Search.CollatorDSO;Extended Properties='Application=Windows';")
+    $objrecordset.open("SELECT System.ItemName, System.ItemTypeText, System.Size,System.ItemPathDisplay  FROM SystemIndex", $objConnection)
+    $objrecordset.MoveFirst()
+    $GLOBAL:SE_results = @()
 
-write-host 'Search Complete'
-write-host $GLOBAL:SE_Results.Length + ' Item/s found , query $SE_Results for details'
+    write-host 'Searching'
 
-$objrecordset.Close()
-$objConnection.Close()
-$objrecordset = $null
-$objConnection = $null
-[gc]::collect()
+    do
+    {
+        $val = ($objrecordset.Fields.Item("System.ItemName"))
+        if($val.value.tostring() -match $searchVal){
+            $GLOBAL:SE_results += $val;
+            $objrecordset.Fields.Item("System.ItemPathDisplay").Value
+        } 
+        #$objrecordset.Fields.Item("System.ITemTypeText")
+        #$objrecordset.Fields.Item("System.SIze")
+        $objrecordset.MoveNext()
+    #    write-host '.' -NoNewLine
+
+    } Until ($objrecordset.EOF)
+
+    write-host 'Search Complete'
+    write-host $GLOBAL:SE_Results.Length + ' Item/s found , query $SE_Results for details'
+
+    $objrecordset.Close()
+    $objConnection.Close()
+    $objrecordset = $null
+    $objConnection = $null
+    [gc]::collect()
+}
