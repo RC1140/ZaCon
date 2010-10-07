0a6edf36b2bfee8fe7dc49529aef2fd67859a712
Jameel <RC1140@republiccommandos.co.za>
2010-10-07 15:52:19 +0200

    Changed encodings for most of the files and renamed them correctly (in most cases)

---
+++ poke-system.ps1
@@ -0,0 +1,41 @@
+function get-users()
+{
+    gwmi win32_useraccount | %{$_}
+}
+
+function get-applications()
+{
+    gwmi Win32_Product
+}
+
+function get-firewallrules()
+{
+    param([string]$rulename='Windows Media')
+    $baseRules = (New-Object -ComObject HNetCfg.FwPolicy2)
+    $baseRules.rules | ? {$_.name -match $rulename}
+}
+
+function get-ADComputers()
+{
+    ([adsisearcher]"objectcategory=computer").findall() | %{([adsi]$_.path).cn + $_.properties.operatingsystemversion}
+
+}
+
+function get-osname()
+{
+    param([parameter(Mandatory=$true)][string]$osversion)
+    switch($osversion)
+    {
+        '6.1'  { 'Windows 7/Windows Server 2008 R2'}
+        '6.0'  { 'Windows Server 2008/Windows Vista'}
+        '5.2'  { 'Windows Server 2003 R2/Windows Server 2003/Windows XP 64-Bit Edition'}
+        '5.1'  { 'Windows XP'}
+        '5.0'  { 'Windows 2000'}
+    }
+}
+
+function get-localOsVersion()
+{
+    get-wmiobject Win32_OperatingSystem | select caption,ServicePackMajorVersion
+}
+
